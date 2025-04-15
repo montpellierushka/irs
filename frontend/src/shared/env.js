59f5467eeec91a60eb3f9c8faecd32e7894bec89
Anton <frameofevil@gmail.com>
2025-04-15 17:40:44 +0700

    https

--- frontend/src/shared/env.js
+++ frontend/src/shared/env.js
@@ -1,6 +1,6 @@
 const Env = {
-    backend: 'http://irs2.irsapp.ru/',   
-    backendMini: 'http://irs2.irsapp.ru',   
+    backend: 'https://irs2.irsapp.ru/',   
+    backendMini: 'https://irs2.irsapp.ru',   
 }
 
 export default Env
